97bb5684573275ccf653bf11cc29a8fdc0f1ea69
zzh8241102 <80749465+zzh8241102@users.noreply.github.com>
2022-12-17 19:02:11 +0800

    finished cookie

--- frontend/src/views/HomePageView.vue
+++ frontend/src/views/HomePageView.vue
@@ -50,14 +50,14 @@
             </div>
         </div>
     </div>
-    <el-dialog v-model="dialogTagManVisible" title="Unfollow the tags you no longer interested" :width="dialogWidthComputed" draggable>
+    <el-dialog v-model="dialogTagManVisible" title="Unfollow the tags you no longer interested"
+        :width="dialogWidthComputed" draggable>
         <center>
             <h5 class="font-setter">Select your tags for Unfollowing</h5>
         </center>
         <br>
         <center>
-            <el-select v-model="tag_selected" placeholder="Select" style="width: 300px"
-                @change="getSelectedTags">
+            <el-select v-model="tag_selected" placeholder="Select" style="width: 300px" @change="getSelectedTags">
                 <el-option v-for="item in tag_options" :value="item" :label="item" :key="item" />
             </el-select>
         </center>
@@ -70,6 +70,16 @@
             </span>
         </template>
     </el-dialog>
+
+    <div id="cookie-area">
+        <div class="ck-style">
+            <div style="padding-bottom:10px;margin-top: 10px;">
+                <cookievue></cookievue>
+            </div>
+            <span style="margin-left: 10px;padding-top:10px;">We use cookies to personalise your experience</span>
+            <el-button type="warning" round class="bt-ck" @click="setCookieUser">Accpet</el-button>
+        </div>
+    </div>
 </template>
 <script setup>
 ////////////////////////////////////////////////
@@ -77,18 +87,18 @@ import NavBar from '../components/NavBar.vue'
 import IndexBlockVue from '../components/IndexBlock.vue';
 import BonfireRec from '../components/BonfireRec.vue'
 import { getUserTags, getTags, unFollowTag } from '../http/api.js'
-import { ref, reactive,onMounted } from 'vue'
+import { ref, reactive, onMounted } from 'vue'
 import QuestionTagVue from '../components/icons/QuestionTag.vue';
 import { ElMessage } from 'element-plus';
-
+import cookievue from '../components/icons/Cookie.vue'
 ////////////////////////////////////////////////
 const dialogWidthComputed = ref('55%');
 const getUserByLocalOrSession = () => {
-  if (sessionStorage.getItem('user_name') != null) {
-    return sessionStorage.getItem('user_name')
-  } else if (localStorage.getItem('user_name') != null) {
-    return localStorage.getItem('user_name')
-  } 
+    if (sessionStorage.getItem('user_name') != null) {
+        return sessionStorage.getItem('user_name')
+    } else if (localStorage.getItem('user_name') != null) {
+        return localStorage.getItem('user_name')
+    }
 }
 
 let tag_options = reactive([])
@@ -151,34 +161,111 @@ const getSelectedTags = (item) => {
 }
 
 
+const setCookieUser = () => {
+    // use cookie to store the user name
+     const cookieArea = document.getElementById('cookie-area')
+    const user_name = getUserByLocalOrSession()
+    let date = new Date()
+    date.setTime(date.getTime() + 30 * 24 * 60 * 60 * 1000)
+    // set the cookie name as user_name+time, value as user_name
+    document.cookie = user_name + '=' + user_name + ';expires=' + date.toGMTString()
 
+    cookieArea.style.display = 'none'
+}
 
 const popCookieWindow = () => {
+    // check if the user has cookie
+    // if cookie value = user_name, then do nothing
+    // else, pop the cookie window
+    const user_name = getUserByLocalOrSession()
+    let cookie_name = user_name
+    let cookie_value = user_name
+    let cookie = document.cookie
+    let cookie_array = cookie.split(';')
+    let cookie_flag = false
+    for (let i = 0; i < cookie_array.length; i++) {
+        let cookie_item = cookie_array[i].split('=')
+        if (cookie_item[0].trim() == cookie_name && cookie_item[1].trim() == cookie_value) {
+            cookie_flag = true
+        }
+    }
+    if(cookie_flag){
+        const cookieArea = document.getElementById('cookie-area')
+        cookieArea.style.display = 'none'
+        return
+    }
+    // id="cookie-area"
+    const cookieArea = document.getElementById('cookie-area')
+    // cookieArea.style.display = 'none'
+    // 渐变动画出现，持续1s
+    cookieArea.style.opacity = 0
+    let op = 0
+    let timer = setInterval(() => {
+        if (op >= 10000) {
+            clearInterval(timer)
+        }
+        cookieArea.style.opacity = op
+        cookieArea.style.filter = 'alpha(opacity=' + op * 1 + ")"
+        op += op * 0.1 + 0.1
+    }, 10)
 
 }
 
 
+
 onMounted(() => {
+    popCookieWindow()
     if (window.innerWidth <= 800) {
-    dialogWidthComputed.value = '80%';
-  } else if (window.innerWidth > 800) {
-    dialogWidthComputed.value = '55%';
-  }
-
-
-  window.onresize = () => {
-    if (window.innerWidth <= 800) {
-      dialogWidthComputed.value = '80%';
+        dialogWidthComputed.value = '80%';
     } else if (window.innerWidth > 800) {
-      dialogWidthComputed.value = '55%';
+        dialogWidthComputed.value = '55%';
     }
-  }
+
+
+    window.onresize = () => {
+        if (window.innerWidth <= 800) {
+            dialogWidthComputed.value = '80%';
+        } else if (window.innerWidth > 800) {
+            dialogWidthComputed.value = '55%';
+        }
+    }
 })
 
 
 ////////////////////////////////////////////////
 </script>
 <style scoped>
+.bt-ck {
+    margin-top: 16px;
+    margin-right: 10px;
+}
+
+.ck-style {
+    margin-top: 1px;
+    margin-left: 5px;
+    display: flex;
+}
+
+#cookie-area {
+
+    display: flex;
+    /* display: none; */
+    position: fixed;
+    /* 在页面下方出现 */
+    bottom: 5px;
+    /* 在页面右方出现 */
+    left: 5px;
+    width: 370px;
+    height: 70px;
+    /* 背景高斯模糊 */
+    background-color: aliceblue;
+    /* 出现的时候从左到右渐变 */
+    border-radius: 5px;
+    /* shadow */
+    box-shadow: 0 0 2px rgba(0, 0, 0, 0.5);
+
+}
+
 .bonfire {
     margin-top: 30px;
 }
@@ -345,22 +432,26 @@ onMounted(() => {
         max-width: 90vw;
 
     }
+
     .tag-area {
         width: 98%;
     }
+
     .tag-manage-bottom {
         position: relative;
         left: 70%;
     }
+
     .left-title {
         margin-right: 17%;
-    }  
-    .tag-banner{
+    }
+
+    .tag-banner {
         margin-left: 0px;
         position: relative;
         left: 70%;
     }
-    
+
 }
 
 /* 屏幕宽度小于790时 */
@@ -375,14 +466,16 @@ onMounted(() => {
         width: 100%;
     }
 
-    .tag-area{
+    .tag-area {
         width: 100%;
         margin: 0px;
     }
-    .recommend-area{
+
+    .recommend-area {
         width: 100%;
         margin: 0px;
     }
+
     .tag-manage-bottom {
         position: relative;
         left: 68%;
@@ -392,17 +485,17 @@ onMounted(() => {
 
 
 
-@media screen and (max-width: 490px)
-{
+@media screen and (max-width: 490px) {
     .tag-manage-bottom {
         position: relative;
         left: 58%;
     }
+
     .big-post {
-    width: 15%;
-    display: inline-block;
-    margin-left: 23%;
-}
+        width: 15%;
+        display: inline-block;
+        margin-left: 23%;
+    }
 
 }
 </style>
